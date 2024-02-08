47b2d4d4cd6fd8871ea57e7d0f3b219162c04726
Gino Abbas <82870344+GinoLeCodeur@users.noreply.github.com>
2024-02-08 22:00:39 +0100

    feat: add recipes overview to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,45 @@
-export default function Home() {
-  return <main className="flex-1 w-full max-w-screen-xl py-4">Hello world</main>;
+import Image from "next/image";
+import Link from "next/link";
+import { Suspense } from "react";
+import { sql } from "@vercel/postgres";
+
+export default async function Home() {
+    const recipesData = await sql`
+        SELECT recipe_id AS "recipeId", name, image FROM recipes
+    `;
+    
+    return (
+        <main className="flex-1 w-full max-w-screen-xl py-4">
+            <section>
+                <Suspense fallback={<p>Loading recipes...</p>}>
+                    <ol className="grid grid-cols-4 gap-4">
+                        {recipesData.rows.map((recipe) => (
+                            <li key={recipe.recipeId}>
+                                <article>
+                                    <Link
+                                        href={`/recipe/${recipe.recipeId}`}
+                                        className="flex flex-col bg-[#fff]"
+                                    >
+                                        <picture>
+                                            <Image
+                                                src="https://placehold.co/600x400?text=Hello+world"
+                                                alt={recipe.name}
+                                                width="600"
+                                                height="400"
+                                                unoptimized
+                                                className="w-full"
+                                            />
+                                        </picture>
+                                        <h2 className="p-4 font-bold">
+                                            {recipe.name}
+                                        </h2>
+                                    </Link>
+                                </article>
+                            </li>
+                        ))}
+                    </ol>
+                </Suspense>
+            </section>
+        </main>
+    );
 }
