609f9e0412de13326790dc61dec416f575e9d580
Gino Abbas <82870344+GinoLeCodeur@users.noreply.github.com>
2024-02-21 21:22:55 +0100

    feat: move main wrapper element from page to layout

--- src/app/recipe/[slug]/page.tsx
+++ src/app/recipe/[slug]/page.tsx
@@ -50,55 +50,53 @@ export default async function Page({
     `;
 
     return (
-        <main className="flex-1 w-full max-w-screen-xl py-4">
-            <article>
-                <header className="mb-4">
-                    <picture>
-                        <Image
-                            src="https://placehold.co/600x400?text=Hello+world"
-                            alt={recipesData[0].name}
-                            width="600"
-                            height="400"
-                            unoptimized
-                            className="w-full h-[300px] object-cover"
-                        />
-                    </picture>
-                </header>
-                <div className="flex items-start gap-4">
-                    <main className="flex-1">
-                        <h1 className="text-4xl font-medium">
-                            {recipesData[0].name}
-                        </h1>
-                        {recipesData[0].description.length > 0 &&
-                            <p>{recipesData[0].description}</p>
-                        }
-                        <h3 className="mt-4 font-bold">Stappen:</h3>
-                        {recipeStepsData.length ? (
-                            recipeStepsData.map((step) => (
-                                <p key={step.recipeStepId}>
-                                    {step.description}
-                                </p>
-                            ))
-                        ) : (
-                            <p>Geen stappen gevonden</p>
-                        )}
-                    </main>
-                    <aside className="sticky top-[0] w-[300px]">
-                        <h4 className="font-bold">Ingrediënten</h4>
-                        {ingredientsData.length ? (
-                            <ol>
-                                {ingredientsData.map((ingredient) => (
-                                    <li key={ingredient.ingredientId}>
-                                        {ingredient.name}
-                                    </li>
-                                ))}
-                            </ol>
-                        ) : (
-                            <p>Geen ingrediënten gevonden</p>
-                        )}
-                    </aside>
-                </div>
-            </article>
-        </main>
+        <article>
+            <header className="mb-4">
+                <picture>
+                    <Image
+                        src="https://placehold.co/600x400?text=Hello+world"
+                        alt={recipesData[0].name}
+                        width="600"
+                        height="400"
+                        unoptimized
+                        className="w-full h-[300px] object-cover"
+                    />
+                </picture>
+            </header>
+            <div className="flex items-start gap-4">
+                <main className="flex-1">
+                    <h1 className="text-4xl font-medium">
+                        {recipesData[0].name}
+                    </h1>
+                    {recipesData[0].description.length > 0 &&
+                        <p>{recipesData[0].description}</p>
+                    }
+                    <h3 className="mt-4 font-bold">Stappen:</h3>
+                    {recipeStepsData.length ? (
+                        recipeStepsData.map((step) => (
+                            <p key={step.recipeStepId}>
+                                {step.description}
+                            </p>
+                        ))
+                    ) : (
+                        <p>Geen stappen gevonden</p>
+                    )}
+                </main>
+                <aside className="sticky top-[0] w-[300px]">
+                    <h4 className="font-bold">Ingrediënten</h4>
+                    {ingredientsData.length ? (
+                        <ol>
+                            {ingredientsData.map((ingredient) => (
+                                <li key={ingredient.ingredientId}>
+                                    {ingredient.name}
+                                </li>
+                            ))}
+                        </ol>
+                    ) : (
+                        <p>Geen ingrediënten gevonden</p>
+                    )}
+                </aside>
+            </div>
+        </article>
     );
 }
