88319477d4b41de23a55ded33c17037ccd780eb4
Gino Abbas <82870344+GinoLeCodeur@users.noreply.github.com>
2024-02-23 16:30:24 +0100

    feat: add improved middleware auth redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,30 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
+import { withAuth } from "next-auth/middleware";
 import { getToken } from 'next-auth/jwt';
 
 const secret = process.env.NEXTAUTH_SECRET;
 
-export async function middleware(req: NextRequest) {
-    const session = await getToken({ req, secret });
+export default withAuth(
+    async function middleware(req: NextRequest) {
+        const session = await getToken({ req, secret });
 
-    if (session && req.nextUrl.pathname === '/login') {
-        return NextResponse.redirect(new URL('/', req.url));
+        if (session && req.nextUrl.pathname === '/login') {
+            return NextResponse.redirect(new URL('/', req.url));
+        }
+    },
+    {
+        callbacks: {
+            authorized: ({ token, req }) =>
+                !!token ||
+                req.nextUrl.pathname.startsWith('/api') ||
+                req.nextUrl.pathname.startsWith('/login'),
+        },
     }
-}
+);
+
+export const config = {
+    matcher: [
+        '/login',
+        '/recipe/manage'
+    ],
+};
